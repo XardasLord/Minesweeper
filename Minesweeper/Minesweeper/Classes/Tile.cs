8605ed833ef4110c5f3409529139d7351e99b1f4
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-06-12 22:15:11 +0200

    Added some base classes like Tile and GameEngine and some basic start logic.

---
+++ Minesweeper/Minesweeper/Classes/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Minesweeper.Classes
+{
+    public class Tile : Button
+    {
+        private static readonly Size _size = new Size(23, 23);
+        public static readonly Point _startPos = new Point(13, 24);
+
+        public int Row { get; }
+        public int Column { get; }
+        public GameEngine Engine { get; }
+
+        public Tile(int row, int column, GameEngine engine)
+        {
+            Row = row;
+            Column = column;
+            Engine = engine;
+            
+            Size = _size;
+            Location = new Point(_startPos.X + _size.Width * Column, _startPos.Y + _size.Height * Row);
+
+            Engine.AddTile(this);
+        }
+    }
+}
